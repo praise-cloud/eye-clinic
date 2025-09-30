65ffa65638d15b94e724cc5603fee4957de5e212
praise-cloud <georgewillpraise1@gmail.com>
2025-09-30 12:24:22 +0100

    update for tailwindcss to work well

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  plugins: {
-    /*tailwindcss: {},*/ "@tailwindcss/postcss": {},
-    autoprefixer: {},
-  },
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
 }
